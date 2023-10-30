e4edccd16f0031227a1f58baf18f0fa4c2c1a2ae
Wadson Nilton <niiltonsk8@gmail.com>
2023-10-29 21:26:29 -0300

    add contribution section

---
+++ src/components/Contribution.svelte
@@ -0,0 +1,69 @@
+<section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto text-center">
+            <h2 class="text-3xl font-bold leading-tight text-gray-800 sm:text-4xl lg:text-4xl">CONTRIBUIDORES <span class="text-teal-600">RD UTILITIES</span></h2>
+        </div>
+
+        <div class="grid max-w-xl grid-cols-1 mx-auto mt-8 text-center lg:max-w-full sm:mt-12 lg:mt-20 lg:grid-cols-3 gap-x-6 xl:gap-x-12 gap-y-6">
+            <div class="overflow-hidden bg-white rounded-md shadow">
+                <div class="px-8 py-12">
+                    <div class="relative w-24 h-24 mx-auto">
+                        <img class="relative object-cover w-24 h-24 mx-auto rounded-full" src="user/andre.jpg" alt="" />
+                        <div class="absolute top-0 right-0 flex items-center justify-center bg-teal-600 rounded-full w-7 h-7">
+                            <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path
+                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <blockquote class="mt-7">
+                        <p class="text-lg text-black italic">“Seria interessante inserir normas tecnicas e  aplicações para manipular PDF. como: Juntar PDF, JPG para PDF”</p>
+                    </blockquote>
+                    <p class="text-base font-semibold tex-tblack mt-9">André Luis Ramos</p>
+                    <p class="mt-1 text-base text-gray-600">Engenheiro Eletricista</p>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-white rounded-md shadow">
+                <div class="px-8 py-12">
+                    <div class="relative w-24 h-24 mx-auto">
+                        <img class="relative object-cover w-24 h-24 mx-auto rounded-full" src="user/werica.jpg" alt="" />
+                        <div class="absolute top-0 right-0 flex items-center justify-center bg-teal-600 rounded-full w-7 h-7">
+                            <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path
+                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <blockquote class="mt-7">
+                        <p class="text-lg text-black italic">“Segurança no Setor Elétrico: Recursos, Ferramentas e Normas para um Ambiente de Trabalho Protegid”</p>
+                    </blockquote>
+                    <p class="text-base font-semibold tex-tblack mt-9">Maria Werica</p>
+                    <p class="mt-1 text-base text-gray-600">Engenheira de Segurança do Trabalho</p>
+                </div>
+            </div>
+
+            <div class="overflow-hidden bg-white rounded-md shadow">
+                <div class="px-8 py-12">
+                    <div class="relative w-24 h-24 mx-auto">
+                        <img class="relative object-cover w-24 h-24 mx-auto rounded-full" src="user/anonymous.jpg" alt="" />
+                        <div class="absolute top-0 right-0 flex items-center justify-center bg-teal-600 rounded-full w-7 h-7">
+                            <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path
+                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <blockquote class="mt-7">
+                        <p class="text-lg text-black italic">“Seria interessante alguma informação relacionado as bitolas dos cabos de MT e BT e estruturas”</p>
+                    </blockquote>
+                    <p class="text-base font-semibold tex-tblack mt-9">Carlos Henrique</p>
+                    <p class="mt-1 text-base text-gray-600">Tecnico do setor Eletrico</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import About from "../components/Rules.svelte";
     import Contact from "../components/Contact.svelte";
     import Footer from "../components/Footer.svelte";
+    import Contribution from "../components/Contribution.svelte";
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
@@ -11,6 +12,7 @@ export const config = {
 
 
 <About />
+<Contribution />
 <Contact />
 <Footer />
 <slot />
